377e064cd131d9c982c887dba4d8b09c0b202187
Marcelo Barboza <26288817+mpbsd@users.noreply.github.com>
2025-05-31 01:59:55 -0300

    encurtar codigos para teclas

--- keyboards/crkbd/keymaps/bezerra/keymap.c
+++ keyboards/crkbd/keymaps/bezerra/keymap.c
@@ -18,23 +18,23 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #include QMK_KEYBOARD_H
 
 // LAYERS
-enum _L { _QW, _NR, _SB, _FN, _AR };
+enum _L { _Q, _N, _S, _F, _A };
 
 // KEYS
 const uint16_t PROGMEM _K[5][3][10] = {
   // QWERTY {{{
   {
     {
-      LT(_AR, KC_Q),
-      LT(_FN, KC_W),
-      LT(_SB, KC_E),
-      LT(_NR, KC_R),
+      LT(_A, KC_Q),
+      LT(_F, KC_W),
+      LT(_S, KC_E),
+      LT(_N, KC_R),
       KC_T,
       KC_Y,
-      LT(_NR, KC_U),
-      LT(_SB, KC_I),
-      LT(_FN, KC_O),
-      LT(_AR, KC_P)
+      LT(_N, KC_U),
+      LT(_S, KC_I),
+      LT(_F, KC_O),
+      LT(_A, KC_P)
     },
     {
       LCTL_T(KC_A),
@@ -229,12 +229,12 @@ const uint16_t PROGMEM _T[5][2][2] = {
   // QWERTY {{{
   {
     {
-      LT(_SB, KC_BSPC),
-      LT(_NR, KC_ESC)
+      LT(_S, KC_BSPC),
+      LT(_N, KC_ESC)
     },
     {
-      LT(_NR, KC_ENT),
-      LT(_SB, KC_SPC)
+      LT(_N, KC_ENT),
+      LT(_S, KC_SPC)
     }
   },
   // }}}
@@ -289,69 +289,69 @@ const uint16_t PROGMEM _T[5][2][2] = {
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-  [_QW] = LAYOUT_split_3x6_3(
-      XXXXXXX, _K[_QW][0][0], _K[_QW][0][1], _K[_QW][0][2], _K[_QW][0][3], _K[_QW][0][4], _K[_QW][0][5], _K[_QW][0][6], _K[_QW][0][7], _K[_QW][0][8], _K[_QW][0][9], XXXXXXX,
-      XXXXXXX, _K[_QW][1][0], _K[_QW][1][1], _K[_QW][1][2], _K[_QW][1][3], _K[_QW][1][4], _K[_QW][1][5], _K[_QW][1][6], _K[_QW][1][7], _K[_QW][1][8], _K[_QW][1][9], XXXXXXX,
-      XXXXXXX, _K[_QW][2][0], _K[_QW][2][1], _K[_QW][2][2], _K[_QW][2][3], _K[_QW][2][4], _K[_QW][2][5], _K[_QW][2][6], _K[_QW][2][7], _K[_QW][2][8], _K[_QW][2][9], XXXXXXX,
-                                                   XXXXXXX, _T[_QW][0][0], _T[_QW][0][1], _T[_QW][1][0], _T[_QW][1][1],       XXXXXXX
+  [_Q] = LAYOUT_split_3x6_3(
+      XXXXXXX, _K[_Q][0][0], _K[_Q][0][1], _K[_Q][0][2], _K[_Q][0][3], _K[_Q][0][4], _K[_Q][0][5], _K[_Q][0][6], _K[_Q][0][7], _K[_Q][0][8], _K[_Q][0][9], XXXXXXX,
+      XXXXXXX, _K[_Q][1][0], _K[_Q][1][1], _K[_Q][1][2], _K[_Q][1][3], _K[_Q][1][4], _K[_Q][1][5], _K[_Q][1][6], _K[_Q][1][7], _K[_Q][1][8], _K[_Q][1][9], XXXXXXX,
+      XXXXXXX, _K[_Q][2][0], _K[_Q][2][1], _K[_Q][2][2], _K[_Q][2][3], _K[_Q][2][4], _K[_Q][2][5], _K[_Q][2][6], _K[_Q][2][7], _K[_Q][2][8], _K[_Q][2][9], XXXXXXX,
+                                                XXXXXXX, _T[_Q][0][0], _T[_Q][0][1], _T[_Q][1][0], _T[_Q][1][1],      XXXXXXX
       ),
-  [_NR] = LAYOUT_split_3x6_3(
-      XXXXXXX, _K[_NR][0][0], _K[_NR][0][1], _K[_NR][0][2], _K[_NR][0][3], _K[_NR][0][4], _K[_NR][0][5], _K[_NR][0][6], _K[_NR][0][7], _K[_NR][0][8], _K[_NR][0][9], XXXXXXX,
-      XXXXXXX, _K[_NR][1][0], _K[_NR][1][1], _K[_NR][1][2], _K[_NR][1][3], _K[_NR][1][4], _K[_NR][1][5], _K[_NR][1][6], _K[_NR][1][7], _K[_NR][1][8], _K[_NR][1][9], XXXXXXX,
-      XXXXXXX, _K[_NR][2][0], _K[_NR][2][1], _K[_NR][2][2], _K[_NR][2][3], _K[_NR][2][4], _K[_NR][2][5], _K[_NR][2][6], _K[_NR][2][7], _K[_NR][2][8], _K[_NR][2][9], XXXXXXX,
-                                                   XXXXXXX, _T[_NR][0][0], _T[_NR][0][1], _T[_NR][1][0], _T[_NR][1][1],       XXXXXXX
+  [_N] = LAYOUT_split_3x6_3(
+      XXXXXXX, _K[_N][0][0], _K[_N][0][1], _K[_N][0][2], _K[_N][0][3], _K[_N][0][4], _K[_N][0][5], _K[_N][0][6], _K[_N][0][7], _K[_N][0][8], _K[_N][0][9], XXXXXXX,
+      XXXXXXX, _K[_N][1][0], _K[_N][1][1], _K[_N][1][2], _K[_N][1][3], _K[_N][1][4], _K[_N][1][5], _K[_N][1][6], _K[_N][1][7], _K[_N][1][8], _K[_N][1][9], XXXXXXX,
+      XXXXXXX, _K[_N][2][0], _K[_N][2][1], _K[_N][2][2], _K[_N][2][3], _K[_N][2][4], _K[_N][2][5], _K[_N][2][6], _K[_N][2][7], _K[_N][2][8], _K[_N][2][9], XXXXXXX,
+                                                XXXXXXX, _T[_N][0][0], _T[_N][0][1], _T[_N][1][0], _T[_N][1][1],      XXXXXXX
       ),
-  [_SB] = LAYOUT_split_3x6_3(
-      XXXXXXX, _K[_SB][0][0], _K[_SB][0][1], _K[_SB][0][2], _K[_SB][0][3], _K[_SB][0][4], _K[_SB][0][5], _K[_SB][0][6], _K[_SB][0][7], _K[_SB][0][8], _K[_SB][0][9], XXXXXXX,
-      XXXXXXX, _K[_SB][1][0], _K[_SB][1][1], _K[_SB][1][2], _K[_SB][1][3], _K[_SB][1][4], _K[_SB][1][5], _K[_SB][1][6], _K[_SB][1][7], _K[_SB][1][8], _K[_SB][1][9], XXXXXXX,
-      XXXXXXX, _K[_SB][2][0], _K[_SB][2][1], _K[_SB][2][2], _K[_SB][2][3], _K[_SB][2][4], _K[_SB][2][5], _K[_SB][2][6], _K[_SB][2][7], _K[_SB][2][8], _K[_SB][2][9], XXXXXXX,
-                                                   XXXXXXX, _T[_SB][0][0], _T[_SB][0][1], _T[_SB][1][0], _T[_SB][1][1],       XXXXXXX
+  [_S] = LAYOUT_split_3x6_3(
+      XXXXXXX, _K[_S][0][0], _K[_S][0][1], _K[_S][0][2], _K[_S][0][3], _K[_S][0][4], _K[_S][0][5], _K[_S][0][6], _K[_S][0][7], _K[_S][0][8], _K[_S][0][9], XXXXXXX,
+      XXXXXXX, _K[_S][1][0], _K[_S][1][1], _K[_S][1][2], _K[_S][1][3], _K[_S][1][4], _K[_S][1][5], _K[_S][1][6], _K[_S][1][7], _K[_S][1][8], _K[_S][1][9], XXXXXXX,
+      XXXXXXX, _K[_S][2][0], _K[_S][2][1], _K[_S][2][2], _K[_S][2][3], _K[_S][2][4], _K[_S][2][5], _K[_S][2][6], _K[_S][2][7], _K[_S][2][8], _K[_S][2][9], XXXXXXX,
+                                                XXXXXXX, _T[_S][0][0], _T[_S][0][1], _T[_S][1][0], _T[_S][1][1],      XXXXXXX
       ),
-  [_FN] = LAYOUT_split_3x6_3(
-      XXXXXXX, _K[_FN][0][0], _K[_FN][0][1], _K[_FN][0][2], _K[_FN][0][3], _K[_FN][0][4], _K[_FN][0][5], _K[_FN][0][6], _K[_FN][0][7], _K[_FN][0][8], _K[_FN][0][9], XXXXXXX,
-      XXXXXXX, _K[_FN][1][0], _K[_FN][1][1], _K[_FN][1][2], _K[_FN][1][3], _K[_FN][1][4], _K[_FN][1][5], _K[_FN][1][6], _K[_FN][1][7], _K[_FN][1][8], _K[_FN][1][9], XXXXXXX,
-      XXXXXXX, _K[_FN][2][0], _K[_FN][2][1], _K[_FN][2][2], _K[_FN][2][3], _K[_FN][2][4], _K[_FN][2][5], _K[_FN][2][6], _K[_FN][2][7], _K[_FN][2][8], _K[_FN][2][9], XXXXXXX,
-                                                   XXXXXXX, _T[_FN][0][0], _T[_FN][0][1], _T[_FN][1][0], _T[_FN][1][1],       XXXXXXX
+  [_F] = LAYOUT_split_3x6_3(
+      XXXXXXX, _K[_F][0][0], _K[_F][0][1], _K[_F][0][2], _K[_F][0][3], _K[_F][0][4], _K[_F][0][5], _K[_F][0][6], _K[_F][0][7], _K[_F][0][8], _K[_F][0][9], XXXXXXX,
+      XXXXXXX, _K[_F][1][0], _K[_F][1][1], _K[_F][1][2], _K[_F][1][3], _K[_F][1][4], _K[_F][1][5], _K[_F][1][6], _K[_F][1][7], _K[_F][1][8], _K[_F][1][9], XXXXXXX,
+      XXXXXXX, _K[_F][2][0], _K[_F][2][1], _K[_F][2][2], _K[_F][2][3], _K[_F][2][4], _K[_F][2][5], _K[_F][2][6], _K[_F][2][7], _K[_F][2][8], _K[_F][2][9], XXXXXXX,
+                                                XXXXXXX, _T[_F][0][0], _T[_F][0][1], _T[_F][1][0], _T[_F][1][1],      XXXXXXX
       ),
-  [_AR] = LAYOUT_split_3x6_3(
-      XXXXXXX, _K[_AR][0][0], _K[_AR][0][1], _K[_AR][0][2], _K[_AR][0][3], _K[_AR][0][4], _K[_AR][0][5], _K[_AR][0][6], _K[_AR][0][7], _K[_AR][0][8], _K[_AR][0][9], XXXXXXX,
-      XXXXXXX, _K[_AR][1][0], _K[_AR][1][1], _K[_AR][1][2], _K[_AR][1][3], _K[_AR][1][4], _K[_AR][1][5], _K[_AR][1][6], _K[_AR][1][7], _K[_AR][1][8], _K[_AR][1][9], XXXXXXX,
-      XXXXXXX, _K[_AR][2][0], _K[_AR][2][1], _K[_AR][2][2], _K[_AR][2][3], _K[_AR][2][4], _K[_AR][2][5], _K[_AR][2][6], _K[_AR][2][7], _K[_AR][2][8], _K[_AR][2][9], XXXXXXX,
-                                                   XXXXXXX, _T[_AR][0][0], _T[_AR][0][1], _T[_AR][1][0], _T[_AR][1][1],       XXXXXXX
+  [_A] = LAYOUT_split_3x6_3(
+      XXXXXXX, _K[_A][0][0], _K[_A][0][1], _K[_A][0][2], _K[_A][0][3], _K[_A][0][4], _K[_A][0][5], _K[_A][0][6], _K[_A][0][7], _K[_A][0][8], _K[_A][0][9], XXXXXXX,
+      XXXXXXX, _K[_A][1][0], _K[_A][1][1], _K[_A][1][2], _K[_A][1][3], _K[_A][1][4], _K[_A][1][5], _K[_A][1][6], _K[_A][1][7], _K[_A][1][8], _K[_A][1][9], XXXXXXX,
+      XXXXXXX, _K[_A][2][0], _K[_A][2][1], _K[_A][2][2], _K[_A][2][3], _K[_A][2][4], _K[_A][2][5], _K[_A][2][6], _K[_A][2][7], _K[_A][2][8], _K[_A][2][9], XXXXXXX,
+                                                XXXXXXX, _T[_A][0][0], _T[_A][0][1], _T[_A][1][0], _T[_A][1][1],      XXXXXXX
       )
 };
 
 #ifdef ENCODER_MAP_ENABLE
 const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][NUM_DIRECTIONS] = {
-    [_QW] =
+    [_Q] =
         {
             ENCODER_CCW_CW(KC_VOLD, KC_VOLU),
             ENCODER_CCW_CW(KC_MPRV, KC_MNXT),
             ENCODER_CCW_CW(RM_VALD, RM_VALU),
             ENCODER_CCW_CW(KC_RGHT, KC_LEFT),
         },
-    [_NR] =
+    [_N] =
         {
             ENCODER_CCW_CW(KC_VOLD, KC_VOLU),
             ENCODER_CCW_CW(KC_MPRV, KC_MNXT),
             ENCODER_CCW_CW(RM_VALD, RM_VALU),
             ENCODER_CCW_CW(KC_RGHT, KC_LEFT),
         },
-    [_SB] =
+    [_S] =
         {
             ENCODER_CCW_CW(KC_VOLD, KC_VOLU),
             ENCODER_CCW_CW(KC_MPRV, KC_MNXT),
             ENCODER_CCW_CW(RM_VALD, RM_VALU),
             ENCODER_CCW_CW(KC_RGHT, KC_LEFT),
         },
-    [_FN] =
+    [_F] =
         {
             ENCODER_CCW_CW(KC_VOLD, KC_VOLU),
             ENCODER_CCW_CW(KC_MPRV, KC_MNXT),
             ENCODER_CCW_CW(RM_VALD, RM_VALU),
             ENCODER_CCW_CW(KC_RGHT, KC_LEFT),
         },
-    [_AR] =
+    [_A] =
         {
             ENCODER_CCW_CW(KC_VOLD, KC_VOLU),
             ENCODER_CCW_CW(KC_MPRV, KC_MNXT),
